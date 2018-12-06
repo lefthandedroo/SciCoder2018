6d0851fc80d861b88e072c023d1ec7b01455c934
Roo <belredbel@googlemail.com>
2018-12-06 11:32:23 +1100

    Update test_convolution.py

---
+++ module/source/spectrum/testdata/test_convolution.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec  6 11:30:20 2018
+
+@author: BallBlueMeercat
+"""
+
+from ..convolution import Convolution
+
+
+# test magnitude
