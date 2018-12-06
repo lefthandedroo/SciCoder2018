42045edda563160875955b607f6b3b9875aa04d4
Shannon Melrose <s.melrose@unsw.edu.au>
2018-12-06 13:47:58 +1100

    Added colours to skymap

--- module/examples/argparse_plot.py
+++ module/examples/argparse_plot.py
@@ -23,6 +23,9 @@ print("Length (flux) is: " + str(len(s.flux)))
 
 skymap = plt.figure(figsize=(12,6))
 ax = skymap.add_subplot(111, projection="mollweide")
-sc = ax.scatter(coord.Angle(s.ra*u.degree).wrap_at(180*u.degree).radian, coord.Angle(s.dec*u.degree).radian)
+sc = ax.scatter(coord.Angle(s.ra*u.degree).wrap_at(180*u.degree).radian, coord.Angle(s.dec*u.degree).radian, 
+				marker="*", s=200, c=s.color('g', 'r'), cmap=plt.get_cmap("rainbow"), vmin=0, vmax=1)
+plt.colorbar(sc, fraction=0.025, pad=0.04)
 ax.grid(True)
+ax.set_axisbelow(True)
 skymap.savefig("skymap.pdf")
